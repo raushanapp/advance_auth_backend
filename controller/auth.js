c927193826ddbb93f746d8374f9dd072c2c5a23b
raushan kuamr <sraushan890@gmail.com>
2022-12-27 14:54:41 +0530

    error handler functional done

--- controller/auth.js
+++ controller/auth.js
@@ -1,5 +1,5 @@
 const User = require("../models/User");
-
+const ErrorResponse=require("../utils/errorResponse")
 exports.register = async(req, res, next) => {
     const { username, email, password } = req.body;
     try {
@@ -13,10 +13,7 @@ exports.register = async(req, res, next) => {
             user
         });
     } catch (err) {
-        res.status(500).json({
-            success: false,
-            err: err.message
-        });
+        next(err);
     }
     // res.send("Register router");
 };
@@ -24,23 +21,19 @@ exports.register = async(req, res, next) => {
 exports.login = async (req, res, next) => {
     const { email, password } = req.body;
     if (!email || !password) {
-        res.status(400).json({
-            success: false,
-            error:"Please Provide email and password"
-        })
+        return next(new ErrorResponse("Please Provide email and password",400));
     }
     try {
         const user = await User.findOne({ email }).select("+password");    // select password and match the password through methods matchpassword
-        if (!user) send.status(404).json({ success: false, error: "Invalid credentials" });
-
+        if (!user) return next(new ErrorResponse("Invalid credentials",401 ));
         const isMatch = await user.matchPassword(password);
-        if (!isMatch) send.status(404).json({ success: false, error: "Invalid credentials" });
+        if (!isMatch) return next(new ErrorResponse("Invalid credentials",401));
 
         res.status(200).json({success:true,token:"fdhjfa123snb"})
     } catch (err) {
-        res.status(500).json({ success: false, err: err.message });
+       next(err);
     }
-    res.send("Login router");
+    // res.send("Login router");
 };
 
 exports.forgotpassword = (req, res, next) => {
--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 require("dotenv").config({ path: "./.env" });
 const express = require("express");
 const connectDB = require("./config/db");
-
+const errorHandler=require("./middleware/error")
 // connect  DB
 connectDB();
 
 const app = express();
 app.use(express.json());
 
-app.use("/api/auth",require("./routes/auth"))
+app.use("/api/auth", require("./routes/auth"));
+
+// errorhandle should be last error handler
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3300;
 
